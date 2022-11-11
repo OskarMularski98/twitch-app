eb9b09e9d1959bc6ec24ca0e8ff9f52043273a5b
Oskar Mularski <oskarmularski98@gmail.com>
2022-11-11 19:01:24 +0100

    created table info

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,118 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-app-bar dark color="purple darken-3" app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-toolbar-title>Twitch TV</v-toolbar-title>
     </v-app-bar>
 
-    <v-main>
-      <router-view/>
+    <v-navigation-drawer v-model="drawer" fixed temporary>
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-main class="purple darken-4">
+      <table-info :items="streamsDetails" :headers="headers"> </table-info>
     </v-main>
   </v-app>
 </template>
 
 <script>
-
+import TableInfo from "./components/TableInfo.vue";
+import axios from "axios";
 export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
+  components: {
+    TableInfo,
+  },
+  data() {
+    return {
+      drawer: null,
+      streams: [],
+      streamsDetails: [],
+      streamerProfile: [],
+      headers: [
+        {
+          text: "Streamer",
+          value: "user_name",
+        },
+        {
+          text: "Game",
+          value: "game_name",
+        },
+        {
+          text: "Viewers",
+          value: "viewer_count",
+        },
+        {
+          text: "Language",
+          value: "language",
+        },
+      ],
+      streamersId: [],
+    };
+  },
+  async created() {
+    await this.loadStreams();
+    await this.loadStreamer();
+    this.refreshStreams();
+  },
+  methods: {
+    async loadStreams() {
+      this.streamersId = [];
+      try {
+        const response = await axios.get(
+          "https://api.twitch.tv/helix/streams?first=100",
+          {
+            headers: {
+              Authorization: "Bearer n8y2uezpvxa0uavp73bye42mk7c43k",
+              "Client-Id": "cauml8m858lhojpgwkhkk3a4ohx071",
+            },
+          }
+        );
+        this.streams = response.data.data;
+        this.streams.forEach((stream) => {
+          this.streamersId.push(stream.user_id);
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async loadStreamer() {
+      try {
+        const response = await axios.get(
+          `https://api.twitch.tv/helix/users?${this.streamersId
+            .map((id) => `id=${id}`)
+            .join("&")}`,
+          {
+            headers: {
+              Authorization: "Bearer n8y2uezpvxa0uavp73bye42mk7c43k",
+              "Client-Id": "cauml8m858lhojpgwkhkk3a4ohx071",
+            },
+          }
+        );
+        this.streamsDetails = response.data.data;
+        console.log(this.streamsDetails);
+        this.streams.forEach((stream) => {
+          this.streamsDetails.forEach((details) => {
+            if (stream.user_id === details.id) {
+              details.user_name = stream.user_name;
+              details.game_name = stream.game_name;
+              details.viewer_count = stream.viewer_count;
+              details.language = stream.language;
+            }
+          });
+        });
+        console.log(this.streams);
+      } catch (error) {
+        console.log("error", error);
+      }
+    },
+    refreshStreams() {
+      const timeOut = 5000;
+      this.isCleared = setInterval(async () => {
+        await this.loadStreams();
+        await this.loadStreamer();
+      }, timeOut);
+    },
+  },
 };
 </script>
